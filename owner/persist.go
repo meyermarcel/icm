c6e9d04c7975d3788e01242aae3458e63121d577
Marcel Meyer <meyermarcel@posteo.de>
2018-02-18 20:56:13 +0100

    Replace JSON file persistence with SQLite

---
+++ owner/persist.go
@@ -0,0 +1,174 @@
+package owner
+
+import (
+	"log"
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"path/filepath"
+	"os"
+	"github.com/mitchellh/go-homedir"
+	"time"
+)
+
+const appDir = ".iso6346"
+const dbName = "iso6346.db"
+const dirPerm = 0700
+
+type Owner struct {
+	code    string
+	company string
+	city    string
+	country string
+}
+
+func (o Owner) Code() string {
+	return o.code
+}
+
+func (o Owner) Company() string {
+	return o.company
+}
+
+func (o Owner) City() string {
+	return o.city
+}
+
+func (o Owner) Country() string {
+	return o.country
+}
+
+func NewOwner(code, company, city, country string) Owner {
+	return Owner{code, company, city, country}
+}
+
+func deleteOwners(db *sql.DB) {
+
+	tx, err := db.Begin()
+	checkErr(err)
+
+	stmt, err := tx.Prepare("DELETE FROM owner")
+	checkErr(err)
+	defer stmt.Close()
+
+	_, err = stmt.Exec()
+	checkErr(err)
+	tx.Commit()
+}
+
+func getUpdatedTime(db *sql.DB) time.Time {
+
+	rows, err := db.Query("SELECT last_updated FROM updated")
+	checkErr(err)
+	defer rows.Close()
+	var lastUpdated time.Time
+	for rows.Next() {
+		err = rows.Scan(&lastUpdated)
+	}
+	err = rows.Err()
+	checkErr(err)
+
+	return lastUpdated
+}
+
+func saveUpdatedTimeNow(db *sql.DB) {
+
+	tx, err := db.Begin()
+	checkErr(err)
+
+	stmtDel, err := tx.Prepare("DELETE FROM updated")
+	checkErr(err)
+	defer stmtDel.Close()
+
+	_, err = stmtDel.Exec()
+	checkErr(err)
+
+	stmtIns, err := tx.Prepare("INSERT INTO updated (last_updated) VALUES (datetime('now'))")
+	checkErr(err)
+	defer stmtIns.Close()
+
+	_, err = stmtIns.Exec()
+	checkErr(err)
+
+	tx.Commit()
+}
+
+func saveOwners(db *sql.DB, owners []Owner) {
+
+	tx, err := db.Begin()
+	checkErr(err)
+
+	stmt, err := tx.Prepare("INSERT INTO owner (code, company, city, country) VALUES (?, ?, ?, ?)")
+	checkErr(err)
+	defer stmt.Close()
+
+	for _, owner := range owners {
+		_, err = stmt.Exec(owner.Code(), owner.Company(), owner.City(), owner.Country())
+		checkErr(err)
+	}
+	tx.Commit()
+}
+
+func GetOwner(db *sql.DB, code Code) Owner {
+
+	stmt, err := db.Prepare("SELECT company, city, country FROM owner WHERE code = ?")
+	checkErr(err)
+	defer stmt.Close()
+
+	var company string
+	var city string
+	var country string
+	err = stmt.QueryRow(code.Value()).Scan(&company, &city, &country)
+	checkErr(err)
+	return NewOwner(code.Value(), company, city, country)
+}
+
+func getPathToAppDir() string {
+	homeDir, err := homedir.Dir()
+
+	checkErr(err)
+	return filepath.Join(homeDir, appDir)
+}
+
+func initDir(path string) string {
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.Mkdir(path, os.ModeDir|dirPerm)
+	}
+	return path
+}
+
+func InitDB() *sql.DB {
+	db, err := sql.Open("sqlite3", filepath.Join(initDir(getPathToAppDir()), dbName))
+	checkErr(err)
+
+	sqlStmtOwner := `
+		CREATE TABLE IF NOT EXISTS owner (
+		  code    TEXT,
+		  company TEXT,
+		  city    TEXT,
+		  country TEXT
+		);
+	`
+	_, err = db.Exec(sqlStmtOwner)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmtOwner)
+	}
+
+	sqlStmtTime := `
+		CREATE TABLE IF NOT EXISTS updated (
+		  last_updated TIMESTAMP
+		);
+	`
+	_, err = db.Exec(sqlStmtTime)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmtOwner)
+	}
+
+	return db
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- owner/update.go
+++ owner/update.go
@@ -3,132 +3,41 @@ package owner
 import (
 	"github.com/PuerkitoBio/goquery"
 	"log"
-	"encoding/json"
-	"github.com/mitchellh/go-homedir"
 	"time"
 	"sort"
-	"os"
-	"io/ioutil"
-	"path/filepath"
-	"fmt"
 )
-
-const appDirectory = ".iso6346"
-const updatedFileName = "updated.json"
-const ownersFileName = "owners.json"
-const dirPerm = 0700
-const filePerm = 0644
-
-type Owners struct {
-	Owners map[string]Owner `json:"owners"`
-}
-
-type Owner struct {
-	Company string `json:"company"`
-	City    string `json:"city"`
-	Country string `json:"country"`
-}
-
-type Updated struct {
-	Updated time.Time `json:"updated"`
-}
-
-type Result struct {
-	owner   Owner
-	message string
-}
-
-func (mru Updated) after(updated Updated) bool {
-
-	return mru.Updated.After(updated.Updated)
-}
-
-func NewOwner(company, city, country string) Owner {
-	return Owner{company, city, country}
-}
 
 func Update() {
 
-	homeDir, err := homedir.Dir()
+	db := InitDB()
+	defer db.Close()
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pathToAppDir := createPathToAppDir(homeDir)
-
-	pathToUpdatedFile := filepath.Join(pathToAppDir, updatedFileName)
-	pathToOwnersFile := filepath.Join(pathToAppDir, ownersFileName)
-
-	remoteUpdated := getMostRecentUpdate()
-	var localUpdated Updated
-
-	file, err := ioutil.ReadFile(pathToUpdatedFile)
-	if err != nil {
-		writeOwners(pathToOwnersFile)
-		writeUpdated(pathToUpdatedFile, Updated{time.Now()})
-		fmt.Println("Updated owner defintions.")
-	} else {
-		json.Unmarshal([]byte(file), &localUpdated)
-
-		if remoteUpdated.after(localUpdated) {
-			writeOwners(pathToOwnersFile)
-			writeUpdated(pathToUpdatedFile, Updated{time.Now()})
-			fmt.Println("Owners updated.")
-		} else {
-			fmt.Println("Owners already up to date.")
-		}
+	if getUpdatedTimeRemote().After(getUpdatedTime(db)) {
+		saveUpdatedTimeNow(db)
+		deleteOwners(db)
+		saveOwners(db, getOwnersRemote("https://www.bic-code.org/bic-letter-search/?searchterm=A"))
 	}
 }
-func createPathToAppDir(homeDir string) string {
 
-	pathToAppDir := filepath.Join(homeDir, appDirectory)
-
-	if _, err := os.Stat(pathToAppDir); os.IsNotExist(err) {
-		os.Mkdir(pathToAppDir, os.ModeDir|dirPerm)
-	}
-
-	return pathToAppDir
-}
-
-func writeUpdated(path string, updated Updated) {
-	bytes, err := json.Marshal(updated)
-	if err != nil {
-		log.Fatalf("Could not marshal %v", updated)
-	}
-	ioutil.WriteFile(path, bytes, filePerm)
-
-}
-
-func writeOwners(path string) {
-	owners := getOwners("https://www.bic-code.org/bic-letter-search/?searchterm=A")
-	bytes, err := json.Marshal(owners)
-
-	if err != nil {
-		log.Fatalf("Could not marshal %v", owners)
-	}
-	ioutil.WriteFile(path, bytes, filePerm)
-}
-
-func getMostRecentUpdate() Updated {
-	recentlyCreated := getMostRecentDate("https://www.bic-code.org/bic-codes/recently-created")
-	recentlyCancelled := getMostRecentDate("https://www.bic-code.org/bic-codes/recently-cancelled")
+func getUpdatedTimeRemote() time.Time {
+	recentlyCreated := getRecentlyDate("https://www.bic-code.org/bic-codes/recently-created")
+	recentlyCancelled := getRecentlyDate("https://www.bic-code.org/bic-codes/recently-cancelled")
 
 	if recentlyCreated.After(recentlyCancelled) {
-		return Updated{recentlyCreated}
+		return recentlyCreated
 	}
-	return Updated{recentlyCancelled}
+	return recentlyCancelled
 }
 
-func getMostRecentDate(url string) time.Time {
+func getRecentlyDate(url string) time.Time {
 
-	mostRecentDate, err := goquery.NewDocument(url)
+	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	const query = "table tbody tr"
-	dates := mostRecentDate.Find(query).FilterFunction(func(i int, selection *goquery.Selection) bool {
+	dates := doc.Find(query).FilterFunction(func(i int, selection *goquery.Selection) bool {
 		return selection.Children().Is("td")
 	}).Map(func(i int, selection *goquery.Selection) string {
 		return selection.Children().First().Text()
@@ -144,7 +53,7 @@ func getMostRecentDate(url string) time.Time {
 		format := "2 Jan 2006"
 		parsedDate, err := time.Parse(format, date)
 		if err != nil {
-			log.Fatalf("Could not parse date '%s' because format '%s' is no longer valid", date, format)
+			log.Fatalf("Could not parse date '%s' because format '%s' does not work", date, format)
 		}
 		parsedDates = append(parsedDates, parsedDate)
 	}
@@ -152,17 +61,16 @@ func getMostRecentDate(url string) time.Time {
 	sort.Slice(parsedDates, func(i, j int) bool { return parsedDates[i].After(parsedDates[j]) })
 
 	return parsedDates[0]
-
 }
 
-func getOwners(url string) Owners {
+func getOwnersRemote(url string) []Owner {
 
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var owners = map[string]Owner{}
+	var owners []Owner
 
 	const query = "tr td[data-label=Code]"
 	doc.Find(query).Each(func(i int, s *goquery.Selection) {
@@ -171,12 +79,12 @@ func getOwners(url string) Owners {
 		city := s.Parent().Find("td[data-label=City]").Text()
 		country := s.Parent().Find("td[data-label=Country]").Text()
 
-		owners[code[0:3]] = NewOwner(company, city, country)
+		owners = append(owners, NewOwner(code[0:3], company, city, country))
 	})
 
 	if len(owners) == 0 {
 		log.Fatalf("Could not find owners in document from url '%s' with query '%s'", url, query)
 	}
 
-	return Owners{owners}
+	return owners
 }
