6bc31387cf4caae971062e68c66fcdc9a8780e64
Marcel Meyer <meyermarcel@posteo.de>
2018-02-23 15:55:08 +0100

    Move Random function to only user generate

--- cont/generate.go
+++ cont/generate.go
@@ -3,14 +3,18 @@ package cont
 import (
 	"iso6346/owner"
 	"iso6346/equip_cat"
-	"iso6346/gen"
+	"time"
+	"math/rand"
 )
+
+var letterRunes = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var numberRunes = []rune("0123456789")
 
 func Generate() Number {
 
-	ownerCode := owner.NewCode(gen.Random(6, gen.LetterRunes))
-	equipCatId := equip_cat.NewId(gen.Random(1, equip_cat.Ids))
-	serialNumber := NewSerialNum(gen.Random(6, gen.NumberRunes))
+	ownerCode := owner.NewCode(random(6, letterRunes))
+	equipCatId := equip_cat.NewId(random(1, equip_cat.Ids))
+	serialNumber := NewSerialNum(random(6, numberRunes))
 
 	checkDigit := CalcCheckDigit(ownerCode,
 		equipCatId,
@@ -19,4 +23,17 @@ func Generate() Number {
 	return NewContNum(ownerCode,
 		equipCatId,
 		serialNumber, checkDigit)
+}
+
+func random(n int, runes []rune) string {
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = runes[rand.Intn(len(runes))]
+	}
+	return string(b)
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
