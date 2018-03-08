ef0247ba6f70bfe84e70d69dd264f346f1898ac6
Marcel Meyer <meyermarcel@posteo.de>
2018-03-08 08:01:44 +0100

    Add documentation for owner parse

--- cmd/owner_parse.go
+++ cmd/owner_parse.go
@@ -21,13 +21,8 @@ import (
 
 var ownerParseCmd = &cobra.Command{
 	Use:   "parse",
-	Short: "Parse a container number",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Parse an owner.",
+	Long: "Parse an owner.",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		ui.PrintOwnerCode(parser.ParseOwnerCodeOptEquipCat(args[0]))
