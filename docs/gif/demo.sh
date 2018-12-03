5b82745a19a34185fa6850e1fdc644ecc3193620
Marcel Meyer <meyermarcel@posteo.de>
2018-12-03 23:13:52 +0100

    Add new gif for commands

--- docs/gif/demo.sh
+++ docs/gif/demo.sh
@@ -3,25 +3,24 @@
 # From https://github.com/paxtonhare/demo-magic
 . demo-magic.sh -n
 DEMO_PROMPT="$ "
-PROMPT_TIMEOUT=2
+PROMPT_TIMEOUT=3
 
 rm -f ~/.icm/config.yml
-rm -f ~/.icm/data/owners.json
-rm -f ~/.icm/data/equipment-category-ids.json
+rm -f ~/.icm/data/owner.json
+rm -f ~/.icm/data/equipment-category-id.json
 
-cp new_owners.json ~/.icm/data/owners.json
+cp new_owner.json ~/.icm/data/owner.json
 
 clear
-pe "icm validate ' btc u_123'"
 pe "icm validate ' btc u_123123-3'"
 echo -n "$DEMO_PROMPT"
 wait
 clear
-pe "icm owner validate ' btc'"
+pe "icm validate ' btc'"
 echo -n "$DEMO_PROMPT"
 wait
 clear
-pe "icm sizetype validate '20N1'"
+pe "icm validate '20N1'"
 echo -n "$DEMO_PROMPT"
 wait
 clear
@@ -33,29 +32,14 @@ pe "icm generate -c 4"
 echo -n "$DEMO_PROMPT"
 wait
 clear
-pe "# Config printed output"
-pe "cat ~/.icm/config.yml"
-echo -n "$DEMO_PROMPT"
-wait
-clear
-pe "git --no-pager diff ~/.icm/config.yml new_config.yml"
-pe "cp new_config.yml ~/.icm/config.yml"
-echo -n "$DEMO_PROMPT"
-wait
-clear
-pe "icm validate 'btc u_123123-3 22g1'"
-pe "icm generate -c 4"
-echo -n "$DEMO_PROMPT"
-wait
-clear
-pe "# Add custom equipment category id"
 pe "icm validate 'btc x 1231232'"
 echo -n "$DEMO_PROMPT"
 wait
 clear
-pe "cat ~/.icm/data/equipment-category-ids.json"
-pe "git --no-pager diff ~/.icm/data/equipment-category-ids.json new_equipment-category-ids.json"
-pe "cp new_equipment-category-ids.json ~/.icm/data/equipment-category-ids.json"
+pe "# Add new equipment category ID in config"
+pe "cat ~/.icm/data/equipment-category-id.json"
+pe "diff ~/.icm/data/equipment-category-id.json new_equipment-category-id.json"
+pe "cp new_equipment-category-id.json ~/.icm/data/equipment-category-id.json"
 echo -n "$DEMO_PROMPT"
 wait
 clear
@@ -64,5 +48,5 @@ echo -n "$DEMO_PROMPT"
 wait
 
 rm -f ~/.icm/config.yml
-rm -f ~/.icm/data/owners.json
-rm -f ~/.icm/data/equipment-category-ids.json
+rm -f ~/.icm/data/owner.json
+rm -f ~/.icm/data/equipment-category-id.json
